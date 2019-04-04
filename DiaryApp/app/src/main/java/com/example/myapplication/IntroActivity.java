84f6452863dffb87475ea289cc919349dda4e5a9
whale14 <zzazzu14@gmail.com>
2019-04-04 18:43:24 +0900

    인트로 구현

---
+++ DiaryApp/app/src/main/java/com/example/myapplication/IntroActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Handler h = new Handler();
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, 2000);
+    }
+}
